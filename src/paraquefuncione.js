c8e456bb069988abfc7e81f3c14a8f9113d03b7c
neampe <nestoramayaperilla@gmail.com>
2020-04-26 13:37:06 -0500

    primerCommit

---
+++ src/paraquefuncione.js
@@ -0,0 +1,5 @@
+import { getParsedCommandLineOfConfigFile } from "typescript"
+
+
+// para que esta solucion funcine se debe ejeucutar el getParsedCommandLineOfConfigFile
+// npm install --save-dev --unsafe-perm node-sass
